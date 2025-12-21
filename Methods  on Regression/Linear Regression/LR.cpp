fff6f9cd7bf38429775acbe8cc2081f72756c627
Md Feroz Shah <shah2207087@stud.kuet.ac.bd>
2025-12-21 20:11:17 +0600

    code modified

--- Methods  on Regression/Linear Regression/LR.cpp
+++ Methods  on Regression/Linear Regression/LR.cpp
@@ -1,70 +1,37 @@
-<<<<<<< HEAD
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 typedef long long LL;
- 
-int main() {
+
+int main()
+{
     ifstream fin("input.txt");
     ofstream fout("output.txt");
-       if (!fin)
+    if (!fin)
     {
         cerr << "Cannot open input file\n";
         return 1;
     }
     int n;
-while (fin>>n)
-{
-     vector<double>x(n),y(n);
-    for(int i=0;i<n;i++)fin>>x[i];
-    for(int i=0;i<n;i++)fin>>y[i];
-    double sop=0,pos=0,sxs=0,SoX=0,SoY=0;
-    for(int i=0;i<n;i++ ){
-        SoX+=x[i];
-        SoY+=y[i];
-        sop+=(x[i]*y[i]);
-        sxs+=(x[i]*x[i]);
-    }
-      pos=SoX*SoY;
-      double b=(n*sop-pos)/(n*sxs-SoX*SoX);
-      double a=SoY/n-b*(SoX/n);
-    
-      fout<<"y="<<a<<"+"<<b<<"x"<<endl;
-}
-
- 
-=======
-#include<bits/stdc++.h>
-using namespace std;
-typedef long long LL;
- 
-int main() {
-    ifstream fin("input.txt");
-    ofstream fout("output.txt");
-       if (!fin)
+    while (fin >> n)
     {
-        cerr << "Cannot open input file\n";
-        return 1;
-    }
-    int n;
-while (fin>>n)
-{
-     vector<double>x(n),y(n);
-    for(int i=0;i<n;i++)fin>>x[i];
-    for(int i=0;i<n;i++)fin>>y[i];
-    double sop=0,pos=0,sxs=0,SoX=0,SoY=0;
-    for(int i=0;i<n;i++ ){
-        SoX+=x[i];
-        SoY+=y[i];
-        sop+=(x[i]*y[i]);
-        sxs+=(x[i]*x[i]);
-    }
-      pos=SoX*SoY;
-      double b=(n*sop-pos)/(n*sxs-SoX*SoX);
-      double a=SoY/n-b*(SoX/n);
-    
-      fout<<"y="<<a<<"+"<<b<<"x"<<endl;
-}
+        vector<double> x(n), y(n);
+        for (int i = 0; i < n; i++)
+            fin >> x[i];
+        for (int i = 0; i < n; i++)
+            fin >> y[i];
+        double sop = 0, pos = 0, sxs = 0, SoX = 0, SoY = 0;
+        for (int i = 0; i < n; i++)
+        {
+            SoX += x[i];
+            SoY += y[i];
+            sop += (x[i] * y[i]);
+            sxs += (x[i] * x[i]);
+        }
+        pos = SoX * SoY;
+        double b = (n * sop - pos) / (n * sxs - SoX * SoX);
+        double a = SoY / n - b * (SoX / n);
 
- 
->>>>>>> 6c3e029852b36c3d79c5700e44fd1b947f9e967c
- return 0; }
+        fout << "y=" << a << "+" << b << "x" << endl;
+    }
+    return 0;
+}
